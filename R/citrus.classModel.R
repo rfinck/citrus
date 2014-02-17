d20c04ce14dbbb386131329816c8c23076d9ca58
Robert Bruggner <rbruggner@gmail.com>
2014-02-16 20:20:30 -0800

    Trying to console users when SAM throws warnings.

--- R/citrus.classModel.R
+++ R/citrus.classModel.R
@@ -89,7 +89,7 @@ citrus.thresholdCVs.model.quick = function(modelType,features,regularizationThre
     }
     errorRates = sapply(1:ncvIterations,paste("citrus.cvIteration",family,sep="."),modelType="glmnet",features=features,labels=labels,regularizationThresholds=typeRegularizationThresholds,nFolds=nFolds,alpha=alpha,standardize=standardize)
   } else if (modelType=="sam"){
-    warning("No thresholds for SAM. This is Normal.")
+    warning("No thresholds for SAM. You may ignore this message.")
     return(NA)
   } else {
     stop(paste("CV for Model type",modelType,"not implemented"))
